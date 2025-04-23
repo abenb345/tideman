d45012a2e98d29302a9d14f15f25f87f313da3f0
Abe G <abenbg12@gmail.com>
2025-04-22 23:02:04 -0400

    So far, we can ask user to uenter candidate names, and lets user input number of voters and their respective ranked votings

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
-size = int(input("Enter number of candidates: "))
+candidate_size = int(input("Enter number of candidates: "))
 
 candidates = []
-for i in range(size):
+for i in range(candidate_size):
     candidate = input(f"Enter candidate ({i+1} of {size}): ")
     candidates.append(candidate)
 
 # x = candidates
 # print(f"{x}")
+voter_size = int(input("Enter number of voters: "))
+rank = input("Rank these candidates in your desired order: ")
+for i in range(voter_size):
+    for j in range(candidate_size):
+        ans = input(f"{j+1}: ")
