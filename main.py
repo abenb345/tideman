771f2228b7269be9f395709c5be24a8cefaaa34d
Abe G <abenbg12@gmail.com>
2025-04-22 22:56:16 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+size = int(input("Enter number of candidates: "))
+
+candidates = []
+for i in range(size):
+    candidate = input(f"Enter candidate ({i+1} of {size}): ")
+    candidates.append(candidate)
+
+# x = candidates
+# print(f"{x}")
