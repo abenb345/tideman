3332a2cc2930a4575fd5dc78464cd273bd672b24
abenb345 <abenbg12@gmail.com>
2025-04-25 14:10:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 
+#Prompt user to enter number of candidates
 candidate_size = int(input("Enter number of candidates: "))
 
+#dclare arrays and hashes to store voters choice and ranks as candidates from most to least favorite
 ranks = []
 array = []
 
 pairs = {}
 pair = {}
 
+#array to store all candidates
 candidates = []
+
+#Make each user enters the candidates 
 for i in range(candidate_size):
     candidate = input(f"Enter candidate ({i+1} of {candidate_size}): ")
     candidates.append(candidate)
 
 x = candidates
-
+#Print all candidates names 
 print(f"{x}")
+
+#ask user number of voters and how each one of them voted
 voter_size = int(input("Enter number of voters: "))
 print("Rank these candidates in your desired order: ")
 for i in range(voter_size):
@@ -23,7 +30,7 @@ for i in range(voter_size):
         ans = input(f"{j+1}: ")
         array.push(ans)
 
-
+#Rest below is to be reviewed soon
 
 name = input("Enter name: ")
 for i in range(len(candidates)):
